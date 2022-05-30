1c04f656a9dac8b4d66d55a2ff89fd9cc3474361
anthonyoliver1 <anthony.silvaoliveira@outlook.com>
2022-05-29 22:27:12 -0300

    feat(Lyrics): mundando folha de estilo

--- pages/lyrics/Lyrics.js
+++ pages/lyrics/Lyrics.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { FlatList, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { useTheme } from '@react-navigation/native';
+import { FlatList, TouchableOpacity, View } from 'react-native';
 import mockMusicData from '../../utils/mockMusicData.json';
+import { Author, Kids, Title } from '../../style';
+import { Container, List } from '../../style/LyricsStyle';
 
 export default function Lyrics({ navigation }) {
-    const { colors } = useTheme();
 
     const gotToMusicText = ({ number, title, music, author }) => {
         const { text, audio } = music;
@@ -24,65 +24,33 @@ export default function Lyrics({ navigation }) {
     }
 
     return (
-        <SafeAreaView style={styles.container}>
-            <View style={styles.list}>
-                <FlatList
-                    data={mockMusicData}
-                    renderItem={({ item, index, separators }) => (
-                        <TouchableOpacity
-                            key={item.number}
-                            onPress={() => gotToMusicText(item)}
-                            onShowUnderlay={separators.highlight}
-                            onHideUnderlay={separators.unhighlight}
-                            activeOpacity={0.4}
-                        >
-                            <View style={[styles.item]}>
-                                <View>
-                                    <Text style={[styles.title, { color: colors.text }]}>{item.title}</Text>
-                                    <Text style={[styles.author]}>{item.author}</Text>
-                                </View>
-                                <>
-                                    {item.kids ?
-                                        <Text style={{ color: '#5bc8f5', fontSize: 16 }}>kids</Text>
-                                        : null
-                                    }
-                                </>
+        <Container>
+            <FlatList
+                data={mockMusicData}
+                renderItem={({ item, index, separators }) => (
+                    <TouchableOpacity
+                        key={item.number}
+                        onPress={() => gotToMusicText(item)}
+                        onShowUnderlay={separators.highlight}
+                        onHideUnderlay={separators.unhighlight}
+                        activeOpacity={0.4}
+                    >
+                        <List>
+                            <View>
+                                <Title>{item.title}</Title>
+                                <Author>{item.author}</Author>
                             </View>
-                        </TouchableOpacity>
-                    )}
-                    keyExtractor={item => item.number}
-                />
-            </View>
-        </SafeAreaView>
+                            <View>
+                                {item.kids ?
+                                    <Kids>kids</Kids>
+                                    : null
+                                }
+                            </View>
+                        </List>
+                    </TouchableOpacity>
+                )}
+                keyExtractor={item => item.number}
+            />
+        </Container>
     );
 };
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        width: '100%',
-        // backgroundColor: 'red'
-    },
-    list: {
-        // backgroundColor: 'yellow',
-        width: '100%',
-        flex: 1,
-    },
-    item: {
-        padding: 15,
-        marginVertical: 3,
-        width: '100%',
-        borderBottomColor: '#5bc8f5',
-        borderBottomWidth: 1,
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center'
-    },
-    title: {
-        fontSize: 16,
-    },
-    author: {
-        color: 'gray'
-    }
-});
