f1363b47556735849eec25d57b313847a17dc9c4
anthonyoliver1 <anthony.silvaoliveira@outlook.com>
2022-05-29 22:28:25 -0300

    feat(Search): mudando folha de estilo e refatorando

--- pages/search/Search.js
+++ pages/search/Search.js
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
-import { Alert, StyleSheet, Text, Pressable, View, TextInput, ToastAndroid, FlatList, TouchableOpacity } from "react-native";
+import { Alert, View, ToastAndroid, FlatList, TouchableOpacity } from "react-native";
 import { typeDevice } from "../../utils/Index";
 import { useTheme } from '@react-navigation/native';
 import mockMusicData from '../../utils/mockMusicData.json';
 import { useToast } from "react-native-toast-notifications";
+import { Container, ListView, SearchButton, SearchInput } from "../../style/SearchStyle";
+import { Author, ButtonTitle, Kids, Title } from "../../style";
+import { List } from "../../style/LyricsStyle";
 
 
 export default function Search({ navigation }) {
@@ -32,7 +35,7 @@ export default function Search({ navigation }) {
                     ]
                 );
 
-            return toast.show(message, 
+            return toast.show(message,
                 {
                     type: "danger",
                     placement: "top",
@@ -57,7 +60,7 @@ export default function Search({ navigation }) {
                     ]
                 );
 
-            return toast.show(notFoundMessage, 
+            return toast.show(notFoundMessage,
                 {
                     type: "warning",
                     placement: "top",
@@ -96,15 +99,14 @@ export default function Search({ navigation }) {
     }
 
     return (
-        <View style={[styles.centeredView, [{ color: colors.text }]]}>
-            <TextInput
-                style={[styles.input, [{ color: colors.text }]]}
+        <Container>
+            <SearchInput
                 placeholder={"Pesquisar música ..."}
                 placeholderTextColor={dark ? '#5a5a5a' : '#c0c0c0'}
                 onChangeText={text => searcMusicTemp(text)}
                 onSubmitEditing={searchMusic}
             />
-            <Pressable
+            <SearchButton
                 style={
                     ({ pressed }) => [
                         {
@@ -112,18 +114,17 @@ export default function Search({ navigation }) {
                                 ? '#24b1ec'
                                 : '#5bc8f5'
                         },
-                        styles.button
                     ]
                 }
                 onPress={() => searchMusic()}
             >
-                <Text style={[styles.textStyle]}>Pesquisar</Text>
-            </Pressable>
+                <ButtonTitle>Pesquisar</ButtonTitle>
+            </SearchButton>
 
-            <View style={[styles.containerList]}>
+            <ListView>
                 <FlatList
                     data={dataMusic}
-                    renderItem={({ item, index,  separators }) => (
+                    renderItem={({ item, index, separators }) => (
                         <TouchableOpacity
                             key={item.number}
                             onPress={() => gotToMusicText(item)}
@@ -131,74 +132,23 @@ export default function Search({ navigation }) {
                             onHideUnderlay={separators.unhighlight}
                             activeOpacity={0.4}
                         >
-                            <View style={[styles.item]}>
+                            <List>
                                 <View>
-                                    <Text style={[styles.title, { color: colors.text }]}>{item.title}</Text>
-                                    <Text style={[styles.author]}>{item.author}</Text>
+                                    <Title>{item.title}</Title>
+                                    <Author>{item.author}</Author>
                                 </View>
-                                <>
+                                <View>
                                     {item.kids ?
-                                        <Text style={{ color: '#5bc8f5', fontSize: 16 }}>kids</Text>
+                                        <Kids>kids</Kids>
                                         : null
                                     }
-                                </>
-                            </View>
+                                </View>
+                            </List>
                         </TouchableOpacity>
                     )}
                     keyExtractor={item => item.number}
-                /> 
-            </View>
-        </View>
+                />
+            </ListView>
+        </Container>
     )
 };
-
-const styles = StyleSheet.create({
-    centeredView: {
-        alignItems: "center",
-        paddingTop: 20,
-        width: '100%',
-        height: '100%'
-    },
-    containerList: {
-        flex:1,
-        width: '100%',
-    },
-    button: {
-        borderRadius: 20,
-        padding: 10,
-        elevation: 2,
-        width: 160,
-        marginBottom: 10,
-        marginTop: 10
-    },
-    textStyle: {
-        color: "white",
-        fontWeight: "bold",
-        textAlign: "center"
-    },
-    input: {
-        borderWidth: 2,
-        borderRadius: 20,
-        borderColor: "#5bc8f5",
-        height: 40,
-        padding: 10,
-        marginBottom: 10,
-        width: 300,
-    },
-    item: {
-        padding: 15,
-        marginVertical: 3,
-        borderBottomColor: '#5bc8f5',
-        borderBottomWidth: 1,
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center'
-    },
-    title: {
-        fontSize: 16,
-    },
-    author: {
-        color: 'gray'
-    },
-});
