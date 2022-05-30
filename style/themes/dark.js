6cc3b0cc85c433d8fac6258a2f6f52a27ae07ba6
anthonyoliver1 <anthony.silvaoliveira@outlook.com>
2022-05-29 22:32:02 -0300

    feat: criando ligth e dark theme

---
+++ style/themes/dark.js
@@ -0,0 +1,15 @@
+export default {
+    // cores internas do app
+    color: '#FFFFFF',
+    background: '#000000',
+    barColor: 'light',
+    modalColor: '#121212',
+    
+    colors: {
+        //cores da navegação do app
+        primary: '#3186f6',
+        card: '#121212',
+        text: '#FFFFFF',
+        border: '#272729'
+    }
+}
---
+++ style/themes/light.js
@@ -0,0 +1,15 @@
+export default {
+    // cores internas do app
+    color: '#000',
+    background: '#F2F2F2',
+    barColor: 'dark',
+    modalColor: '#FFFFFF',
+
+    colors: {
+        //cores da navegação do app
+        primary: '#3186f6',
+        card: '#FFFFFF',
+        text: '#000',
+        border: '#D8D8D8'
+    }
+}
