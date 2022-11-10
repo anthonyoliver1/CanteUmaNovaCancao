160cfbf84a1ed125ee2844440553f7d1d22bebd9
anthonyoliver1 <anthony.silvaoliveira@outlook.com>
2022-11-10 15:44:38 -0300

    feat: criando camada http

---
+++ service/http.js
@@ -0,0 +1,11 @@
+import { callApi } from "./api";
+
+const BASE_URL = __DEV__ ? 'http://192.168.0.100:5000/' : '';
+
+export const getAllMusics = async () => {
+    return await callApi(
+        BASE_URL,
+        'api/cunc/get-all-musics',
+        'GET'
+    )
+};
