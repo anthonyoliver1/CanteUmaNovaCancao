c25f8f9a0584536f07a8c4aa6a4c737eb37d0f6b
Anthony Oliveira <anthony.silvaoliveira@outlook.com>
2023-01-20 22:19:51 -0300

    feat: definindo cores do toast notification

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import Router from './src/routes/router';
 import { ToastProvider } from 'react-native-toast-notifications'
-import { Dimensions, Keyboard } from 'react-native';
+import { Keyboard } from 'react-native';
+import themes from './src/style/themes'
 
 export default function App() {
   const [positionToast, setPositionToast] = useState(85);
@@ -30,6 +31,10 @@ export default function App() {
       duration={2500}
       animationDuration={200}
       animationType='zoom-in'
+      dangerColor={themes.dark.toastColor.danger}
+      warningColor={themes.dark.toastColor.warning}
+      successColor={themes.dark.toastColor.success}
+      normalColor={themes.dark.toastColor.normal}
     >
       <Router />
     </ToastProvider>
--- src/style/themes/dark.js
+++ src/style/themes/dark.js
@@ -11,5 +11,11 @@ export default {
         card: '#1A1A1A',
         text: '#FFFFFFFF',
         border: '#2A2A2A',
+    },
+    toastColor: {
+        danger: '#E20A0A',
+        warning: '#FF6900',
+        success: '#37BB1A',
+        normal: '#121212'
     }
 }
