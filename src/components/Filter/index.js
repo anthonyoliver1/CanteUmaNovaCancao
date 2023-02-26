b0ef85c70ae4700795a94cf0c8fd163418c77c9f
Anthony Oliveira <anthony.silvaoliveira@outlook.com>
2023-02-26 18:54:18 -0300

    feat(Filter): novo layout dos filtros

--- src/components/Filter/index.js
+++ src/components/Filter/index.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import { FlatList, TouchableOpacity } from 'react-native';
-import { Container, TextButton, Wrapper } from '../../style/FilterStyle';
+import { Container, FilterContainer, TextButton, Wrapper } from '../../style/FilterStyle';
 
 export default function Filter({ listHandler, filterOrder }) {
 
     const renderItem = ({ item, separators }) => (
-        <TouchableOpacity
-            key={item.title}
-            onPress={() => listHandler(item)}
-            onShowUnderlay={separators.highlight}
-            onHideUnderlay={separators.unhighlight}
-            activeOpacity={0.4}
-        >
-            <Wrapper>
-                <TextButton>
-                    {item.title}
-                </TextButton>
-            </Wrapper>
-        </TouchableOpacity>
+        <FilterContainer>
+            <TouchableOpacity
+                key={item.title}
+                onPress={() => listHandler(item)}
+                onShowUnderlay={separators.highlight}
+                onHideUnderlay={separators.unhighlight}
+                activeOpacity={0.4}
+            >
+                <Wrapper selected={item.selected}>
+                    <TextButton selected={item.selected}>
+                        {item.title}
+                    </TextButton>
+                </Wrapper>
+            </TouchableOpacity>
+        </FilterContainer>
     )
 
     return (
--- src/style/FilterStyle.js
+++ src/style/FilterStyle.js
@@ -1,23 +1,37 @@
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
 
 export const Container = styled.View`
-    width: 100%;
     padding: 7px 5px 7px;
+`;
+
+export const FilterContainer = styled.View`
     display: flex;
     justify-content: center;
-    align-content: center;
-    align-items: center;
 `;
 
 export const Wrapper = styled.View`
-    border: solid 1px ${props => props.theme.colors.border};
+    background-color: #1A1A1A7A;
     border-radius: 20px;
     padding: 10px 20px;
-    background-color: ${props => props.theme.colors.card};
     margin-horizontal: 4px;
+
+    ${props =>
+        props.selected && css`
+            background-color: ${props => props.theme.colors.card};
+            padding: 15px 25px;
+            border-radius: 30px;
+        `
+    }
 `;
 
 export const TextButton = styled.Text`
-    color: #FFFFFF;
+    color: #FFFFFFA9;
     font-size: 13px;
+
+    ${props =>
+        props.selected && css`
+            color: #FFFFFF;
+            font-weight: 700;
+        `
+    }
 `;
