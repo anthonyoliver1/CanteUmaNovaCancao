884ce8989a328eda3358c0eb249b3f55dca7b0eb
Anthony Oliveira <anthony.silvaoliveira@outlook.com>
2023-05-02 09:56:02 -0300

    refactor(styles): removendo propriedades css que não são usadas

--- src/style/MusicStyle.js
+++ src/style/MusicStyle.js
@@ -2,8 +2,7 @@ import styled from "styled-components/native";
 
 export const Container = styled.View`
     flex: 1;
-    padding: 10px;
-    align-items: center;
+    padding: 20px;
     min-width: 100%;
     height: 100%;
 `;
